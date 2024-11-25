619ce821d4e50e3b36a7a7f649667c0bd3668e52
dannybrownlee-png <danbrownleephone@gmail.com>
2024-11-24 18:47:55 -0800

    Update lab12.sql
    Exercise 1-8 completed

--- lab12.sql
+++ lab12.sql
@@ -53,3 +53,48 @@ WHERE status != 'Completed'
   AND course_id LIKE 'COMM%'
   AND due_date < '2024-12-31'
 ORDER BY due_date;
+
+--Exercise 1
+SELECT concat(course_name, ' - ', semester)
+FROM courses;
+
+--Exercise 2
+SELECT course_id, course_name, lab_time
+FROM courses
+WHERE lab_time LIKE 'Fri%'
+--Initally I used Not NULL to find out I needed to use LIKE 'Fri%'
+
+--Exercise 3
+SELECT course_id, title, due_date
+FROM assignments
+WHERE due_date > '2024-11-24';
+
+--Exercise 4
+SELECT status, COUNT(*)
+FROM assignments
+GROUP BY status
+
+--Exercise 5
+SELECT course_id, course_name
+FROM courses
+ORDER BY LENGTH(course_name)DESC
+--Descending filter is used to find the longest name
+LIMIT 1
+--Limit to the top result because it's going to be the longest due to the above filter
+
+
+-Exercise 6
+SELECT upper(course_name)
+from courses
+
+--Exercise 7
+SELECT course_id, title, due_date
+FROM assignments
+WHERE due_date LIKE '%-09-%';
+
+-Exercise 8
+SELECT course_id, title, due_date
+FROM assignments
+WHERE due_date IS NULL;
+
+

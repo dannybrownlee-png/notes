c075befd2214d585fbb89bfda6eebd484a78b730
dannybrownlee-png <danbrownleephone@gmail.com>
2024-11-24 18:00:41 -0800

    Update lab11.sql
    Exercises 1-6 completed

--- lab11.sql
+++ lab11.sql
@@ -24,3 +24,32 @@ WHERE status != 'Completed'
   AND course_id LIKE 'COMM%'
   AND due_date < '2024-12-31'
 ORDER BY due_date;
+
+--Exercise 1
+SELECT title, due_date
+FROM assignments
+WHERE course_id LIKE 'COMP1234';
+--Using 'like isn't needed if specific course id is used
+
+--Exercise 2
+SELECT MIN(due_date)
+FROM assignments;
+
+--Exercise 3
+SELECT MAX(due_date)
+FROM assignments;
+
+--Exercise 4 
+SELECT title, course_id
+FROM assignments
+WHERE due_date = '2024-10-08'
+
+--Exercise 5
+SELECT title, due_date
+FROM assignments
+WHERE due_date LIKE '2024-10%';
+
+-Exercise 6
+SELECT MAX(due_date)
+FROM assignments
+WHERE status = 'Completed';
